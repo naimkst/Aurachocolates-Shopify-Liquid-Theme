41ecece84c1649d884c71ea1e8b83266bdb012b8
naimkst <naimkst@gmail.com>
2023-04-11 14:43:00 +0600

    Single product page doen

--- assets/custom.js
+++ assets/custom.js
@@ -63,4 +63,39 @@
   $("#priceSlider").on("input change", function (e) {
     $("#maxValue").text(String(e.target.value));
   });
+
+  /*------------------------------------------
+        = SHOP DETAILS PAGE PRODUCT SLIDER
+    -------------------------------------------*/
+  if ($(".shop-single-slider").length) {
+    $(".slider-for").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: ".slider-nav",
+    });
+    $(".slider-nav").slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      asNavFor: ".slider-for",
+      focusOnSelect: true,
+
+      responsive: [
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 3,
+            infinite: true,
+          },
+        },
+        {
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+      ],
+    });
+  }
 })(window.jQuery);
