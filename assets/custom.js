2645d79208c4abe69f6a75d4fee82db04addf5dc
naimkst <naimkst@gmail.com>
2023-05-01 21:55:30 +0600

    Update custom template

--- assets/custom.js
+++ assets/custom.js
@@ -48,10 +48,6 @@ function openPages(pageName, elmnt, color) {
 }
 
 function getImg(id, img, option) {
-  console.log(id, img, option, image);
-
-  console.log(option);
-
   if (option == "Tray One Box") {
     var image = document.querySelector("#image1");
     var image2 = document.querySelector("#image4");
@@ -94,14 +90,6 @@ function getImgFour(id, img, option) {
   if (option == "Tray Two Box") {
     var image = document.querySelector("#image2");
     var image2 = document.querySelector("#image5");
-    image.src = img;
-    image2.src = img;
-    image.style.opacity = "1";
-    image2.style.opacity = "1";
-  }
-  if (option == "Tray Three Box") {
-    var image = document.querySelector("#image3");
-    var image2 = document.querySelector("#image6");
     image.src = img;
     image2.src = img;
     image.style.opacity = "1";
